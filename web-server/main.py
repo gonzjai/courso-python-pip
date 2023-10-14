7c8a7639d50ee6373a62aff4d045379d04367ba7
gonzjai <gonzalezaraquejairojose@gmail.com>
2023-10-13 20:01:01 -0400

    make use of Fastapi

--- web-server/main.py
+++ web-server/main.py
@@ -6,13 +6,14 @@ app = FastAPI()
 
 @app.get('/')
 def get_list():
-    A = [1,2,3,4]
+    return [1,2,3,4]
 
-@app.get(/contact, response_class=HTMLResponse)
+@app.get('/contact', response_class=HTMLResponse)
 def get_list():
     return """
     <h1>Hola, Esta es mi pagina</h1>
     <p>Y estoy aprendiendo con ella</p>
+    <p>Me parece genial que se reecargue automaticamente al usar uvicorn main:app --reload</p>
     """
 def run():
     store.get_categories()
